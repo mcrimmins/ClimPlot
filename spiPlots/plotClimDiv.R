b1dcbaaf16adb50e83a2fb373e42b2be8abc6ee1
Mike Crimmins <crimmins@email.arizona.edu>
2019-12-06 17:34:02 +0000

    iterative updates

--- spiPlots/plotClimDiv.R
+++ spiPlots/plotClimDiv.R
@@ -6,7 +6,8 @@ library(reshape2)
 library(RCurl)
 library(maps)
 library(raster)
-library(ggplot2)
+#library(ggplot2)
+library(cowplot)
 library(tidyverse)
 #library(zoo)
 library(maptools)
@@ -14,7 +15,8 @@ library(SPEI)
 library(weathermetrics)
 #library(metR)
 library(scales)
-
+library(magick)
+library(plotly)
 # ---- Functions ----
 # capitalize county names
 CapStr <- function(y) {
@@ -35,6 +37,8 @@ countiesPoly<-getData('GADM', country='USA', level=2)
 statesPoly<-getData('GADM', country='USA', level=1)
 # load cdiv shapefile
 cdivPoly<-readShapePoly("/home/crimmins/RProjects/ClimPlot/spiPlots/climDivShp/GIS.OFFICIAL_CLIM_DIVISIONS")
+# ggplot inset data
+states4map <- map_data("state")
 
 # state codes
 stateCodes<-data.frame(code=c(seq(1,48,1),50,seq(101,110,1)),
@@ -110,11 +114,11 @@ for(i in 1:length(dataSets)){
 # ----
   
 # ---- subset and wrangle data ----  
-# county, division or state?
-  region<-"cy" # cy,st, dv
+# county, division or state? DEAL WITH SPECIAL REGIONS!!
+  region<-"st" # cy,st, dv
     state <-"arizona"
       stCode<- stateCodes[which(stateCodes$name==CapStr(state)),1]
-    cdiv  <- 7
+    cdiv  <- 1
     county<- "pima"  
       cyFIPS<-as.character(county.fips[which(county.fips$polyname==paste0(state,",",county)),1])
       cyFIPS<-as.numeric(ifelse(nchar(cyFIPS)==4, substr(cyFIPS,2,4), substr(cyFIPS,3,5)))
@@ -130,6 +134,8 @@ for(i in 1:length(dataSets)){
     # centroid
     subArea<-subset(countiesPoly,NAME_2==CapStr(county) & NAME_1==CapStr(state))
     centroid<-coordinates(subArea)
+    # build name string
+    titleName<-paste0(CapStr(county)," County,",CapStr(state))
   }else if (region=="st"){
     tempData$state<-(as.numeric(substr(tempData$code, 1,3)))
     tempData$div<-(as.numeric(substr(tempData$code, 4,4)))
@@ -139,6 +145,8 @@ for(i in 1:length(dataSets)){
     # centroid
     subArea<-subset(statesPoly, NAME_1==CapStr(state))
     centroid<-coordinates(subArea)
+    # build name string
+    titleName<-paste0(CapStr(state))
   }else{
     tempData$state<-(as.numeric(substr(tempData$code, 1,2)))
     tempData$div<-(as.numeric(substr(tempData$code, 3,4)))
@@ -148,6 +156,8 @@ for(i in 1:length(dataSets)){
     # centroid
     subArea<-subset(cdivPoly, STATE==CapStr(state) & CD_NEW==cdiv)
     centroid<-coordinates(subArea)
+    # build name string
+    titleName<-paste0(CapStr(state)," Climate Division ", cdiv)
   }
 
   # melt data
@@ -165,6 +175,24 @@ for(i in 1:length(dataSets)){
   # calc tmean
   tempDataMelt$tmean<-(tempDataMelt$tmax+tempDataMelt$tmin)/2
 # ----
+  
+# inset map ---- fix MI boundary
+  insetmap<-ggplot() +
+    geom_polygon(data = states4map, aes(x = long, y = lat, group = group), fill="lightgrey", color="grey",size=0.15)  + # get the state border back on top
+    geom_polygon(data = subArea, aes(x = long, y = lat, group = group), fill="orange", color="red", size=0.15)  + # get the state border back on top
+    coord_fixed(xlim=c(-125, -68), ylim = c(25,50), ratio = 1)+
+    #coord_fixed(xlim=c(out$meta$ll[1]-3.5, out$meta$ll[1]+3.5), ylim=c(out$meta$ll[2]-3.5, out$meta$ll[2]+3.5), ratio = 1) +
+    #geom_point(data = point, aes(x = V1, y = V2), size=1, color='red')+
+    theme_bw(base_size=5)+
+      theme(axis.title.x=element_blank(),
+            axis.text.x=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.title.y=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank())+
+      theme(plot.margin = unit(c(0, 0, 0, 0), "pt"))+
+      theme(panel.grid.minor = element_blank(),
+            panel.grid.major = element_blank())
   
 # calculate indices ----
     ## Loop thru full SPI set
@@ -207,57 +235,272 @@ for(i in 1:length(dataSets)){
     tempDataMelt$P_PETAnom <- tempDataMelt$P_PET-tempDataMelt$moAvgP_PET
     # anom sign
     tempDataMelt$pAnomSign<-ifelse(tempDataMelt$precipAnom > 0, "pos", "neg")
+    tempDataMelt$petAnomSign<-ifelse(tempDataMelt$P_PETAnom > 0, "pos", "neg")
     
     # plot variables 
-    date1<-"1980-01-01" # by month
+    date1<-"2010-01-01" # by month
     date2<-"2019-10-01" # by month
     maxScale<-60+1 # max 60
       
-    # SPI contou plot
+# SPI contour plot ----
     dfSPI<-melt(dfSPI, id.vars=c(1:3), measure.vars = 4:63)
     dfSPI$value<-as.numeric(dfSPI$value)
       colnames(dfSPI)[2]<-"month"
-    # plotting
-     ggplot(dfSPI, aes((date),as.numeric(variable) , fill = value))+
+    # current heat map
+      currDfSPI<-dfSPI[which(dfSPI$date==date2),]
+    # plot  
+     pCurr<- ggplot(currDfSPI, aes((date),as.numeric(variable) , fill = value))+
+        geom_tile(width=1)+
+        scale_fill_gradientn(colors=c("orange3","orange","yellow","white","green","green2","darkgreen"), name=" ",
+                             na.value = "grey50", guide = FALSE, limits=c(-3, 3), oob=squish)+
+        theme_bw()+
+        scale_y_continuous(limits=c(0,maxScale), expand=c(0,0), breaks=seq(0,60,6))+
+        scale_x_date(labels = date_format("%b%Y"), expand=c(0,0))+
+        theme(axis.title.y=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks.y=element_blank(),
+              axis.title.x=element_blank())+
+       labs(title=" ")+
+       theme(plot.margin = unit(c(5, 5, 0, 0), "pt"))
+      
+    # main plot  
+    p1<-  ggplot(dfSPI, aes((date),as.numeric(variable) , fill = value))+
       geom_tile(width=31)+
       #scale_fill_gradient2(low = "brown", high = "green",mid = "white",
       #                    na.value = "grey50", guide = "colourbar", limits=c(-3, 3), oob=squish)+
-      scale_fill_gradientn(colors=c("brown","yellow","white","green","darkgreen"),
+      scale_fill_gradientn(colors=c("orange3","orange","yellow","white","green","green2","darkgreen"), name=" ",
                            na.value = "grey50", guide = "colourbar", limits=c(-3, 3), oob=squish)+
-      scale_x_date(labels = date_format("%Y-%m"), breaks='1 years', expand=c(0,0),
+      scale_x_date(labels = date_format("%Y-%m"), breaks='2 years', expand=c(0,0),
                    limits = c(as.Date(date1),as.Date(date2)))+
-      scale_y_continuous(limits=c(1,maxScale), expand=c(0,0), breaks=seq(0,60,6))+
+      scale_y_continuous(limits=c(0,maxScale), expand=c(0,0), breaks=seq(0,60,6))+
       theme_bw()+
-      theme(legend.position="top")
+      theme(legend.position="left")+
+      theme(plot.title = element_text(face="bold"),
+            axis.title.x=element_blank(),
+            axis.text.x=element_blank(),
+            axis.ticks.x=element_blank())+
+      guides(fill = guide_colourbar(barwidth = 1, barheight = 10))+
+      ylab("Timescale (mos)")+
+      labs(title=paste0(titleName," Standardized Precipitation Index (", format(as.Date(date1), "%b%Y"),
+                        " - ",format(as.Date(date2), "%b%Y"),")"))+
+      theme(plot.margin = unit(c(5, 0, 0, 0), "pt"))
+    
     # precip anoms
-     ggplot(tempDataMelt,aes(date, precipAnom, fill = pAnomSign)) + 
+    p2<- ggplot(tempDataMelt,aes(date, precipAnom, fill = pAnomSign)) + 
        geom_bar(stat = "identity", show.legend = FALSE) + 
-       scale_x_date(date_breaks = "1 years", date_labels = "%b") +
        #scale_y_continuous(breaks = seq(-100, 100, 20)) +
-       scale_fill_manual(values = c("brown", "darkgreen"))+
-       theme_bw()
+       scale_fill_manual(values = c("orange4", "darkgreen"))+
+       scale_x_date(labels = date_format("%Y"), breaks='2 years', expand=c(0,0),
+                    limits = c(as.Date(date1),as.Date(date2)))+
+       ylab("Precip Anom (in.)")+
+       xlab("Month-Year")+
+       theme_bw()+
+       theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+       theme(plot.margin = unit(c(0, 0, 0, 0), "pt"))+
+       theme(panel.grid.minor = element_blank())
      
-     
+    # # trying to get alignments
+    #    mainP<-plot_grid(p1, p2, ncol = 1, align = 'v', axis=c('l'),rel_heights = c(3.5,1))
+    #    sideP<-plot_grid(pCurr, NULL, ncol = 1, rel_heights = c(3.5,1))
+    #    plot_grid(mainP, sideP, nrow = 1, align='h',axis = c('tblr'), rel_widths = c(20,1))
+    # # another solution
+    #    plot_grid(p1,pCurr,p2,NULL, ncol = 2, nrow = 2, align = 'v',axis = 'b', rel_heights = c(10,10,1,1),
+    #              rel_widths = c(20,1,20,1))
+  
+    # plotting grid using align
+       mainCurr <- align_plots(p1, pCurr, align = 'h', axis = 'l')
+       mainPrec <- align_plots(p1, p2, align = 'v', axis = 'b')
+       
+       mainP<-plot_grid(mainCurr[[1]], mainPrec[[2]], ncol = 1, align = 'v', axis=c('l'),rel_heights = c(3.5,1))
+       sideP<-plot_grid(pCurr, NULL, ncol = 1, rel_heights = c(3.5,1))
+       #plot_grid(mainP, sideP, nrow = 1, align='h',axis = c('tblr'), rel_widths = c(20,1))
+       spiPlot<-plot_grid(mainP, sideP, nrow = 1, rel_widths = c(20,1))
+  
+       # add inset map
+       spiPlot<-ggdraw(spiPlot)+draw_plot(insetmap, -0.315, 0.40, scale=0.14)
+       
+       # add margin
+       spiPlot = spiPlot + theme(plot.margin = unit(c(0.25, 0.25, 0.7, 0.25), "in")) 
+       # add caption
+       captionString <- c( "Data from NOAA-NCEI",
+                           "ftp://ftp.ncdc.noaa.gov/pub/data/cirs/climdiv/",
+                           paste0("Plot created: ", format(Sys.Date(), "%m-%d-%Y")),
+                           "The University of Arizona",
+                           "https://cals.arizona.edu/climate/")
+       spiPlot<-ggdraw(spiPlot) + draw_text(captionString, x =0.125, 
+                                              y = c(0.0625,0.0500,0.0375,0.0250,0.0125), hjust = 0,vjust=-0.25, size=8)
+       
+ 
+    # write high res to file ----
+       png("/home/crimmins/RProjects/ClimPlot/spiPlots/spiPlots.png", width = 11, height = 8.5, units = "in", res = 300L)
+       #grid.newpage()
+       print(spiPlot, newpage = FALSE)
+       dev.off()
+       
+       # add logos
+       # Call back the plot
+       plot <- image_read("/home/crimmins/RProjects/ClimPlot/spiPlots/spiPlots.png")
+       # And bring in a logo
+       #logo_raw <- image_read("./logos/UA_CLIMAS_logos.png") 
+       logo_raw <- image_read("/home/crimmins/RProjects/ClimPlot/logos/UA_CSAP_CLIMAS_logos_horiz.png") 
+       logo <- image_resize(logo_raw, geometry_size_percent(width=95,height = 95))
+       # Stack them on top of each other
+       #final_plot <- image_append((c(plot, logo)), stack = TRUE)
+       #final_plot <- image_mosaic((c(plot, logo)))
+       final_plot <- image_composite(plot, logo, offset = "+2235+2365")
+       # And overwrite the plot without a logo
+       image_write(final_plot, "/home/crimmins/RProjects/ClimPlot/spiPlots/spiPlots.png")  
+    # ----   
+       
+# PLOTLY SPI HEATMAP ----
+         plot_ly(dfSPI, x = ~date, y = ~variable, z = ~value, colors='BrBG', type = "heatmap", zmin=-3, zmax=3) %>%
+           layout(title = paste0(titleName," Standardized Precipitation Index (", format(as.Date(date1), "%b%Y"),
+                                 " - ",format(as.Date(date2), "%b%Y"),")"),
+                  xaxis=list(title="Month-Year",
+                             range = c(as.Date(date1),as.Date(date2))),
+                  yaxis=list(title="Timescale (mos)",
+                             range = c(0,maxScale))
+           )
+# ----         
+
+       
+# SPEI contour plot ----
+  dfSPEI<-melt(dfSPEI, id.vars=c(1:3), measure.vars = 4:63)
+  dfSPEI$value<-as.numeric(dfSPEI$value)
+  colnames(dfSPEI)[2]<-"month"
+       # current heat map
+       currDfSPEI<-dfSPEI[which(dfSPEI$date==date2),]
+       # plot  
+       pCurr<- ggplot(currDfSPEI, aes((date),as.numeric(variable) , fill = value))+
+         geom_tile(width=1)+
+         scale_fill_gradientn(colors=c("orange3","orange","yellow","white","green","green2","darkgreen"), name=" ",
+                              na.value = "grey50", guide = FALSE, limits=c(-3, 3), oob=squish)+
+         theme_bw()+
+         scale_y_continuous(limits=c(0,maxScale), expand=c(0,0), breaks=seq(0,60,6))+
+         scale_x_date(labels = date_format("%b%Y"), expand=c(0,0))+
+         theme(axis.title.y=element_blank(),
+               axis.text.y=element_blank(),
+               axis.ticks.y=element_blank(),
+               axis.title.x=element_blank())+
+         labs(title=" ")+
+         theme(plot.margin = unit(c(5, 5, 0, 0), "pt"))
+       
+       # main plot  
+       p1<-  ggplot(dfSPEI, aes((date),as.numeric(variable) , fill = value))+
+         geom_tile(width=31)+
+         #scale_fill_gradient2(low = "brown", high = "green",mid = "white",
+         #                    na.value = "grey50", guide = "colourbar", limits=c(-3, 3), oob=squish)+
+         scale_fill_gradientn(colors=c("orange3","orange","yellow","white","green","green2","darkgreen"), name=" ",
+                              na.value = "grey50", guide = "colourbar", limits=c(-3, 3), oob=squish)+
+         scale_x_date(labels = date_format("%Y-%m"), breaks='2 years', expand=c(0,0),
+                      limits = c(as.Date(date1),as.Date(date2)))+
+         scale_y_continuous(limits=c(0,maxScale), expand=c(0,0), breaks=seq(0,60,6))+
+         theme_bw()+
+         theme(legend.position="left")+
+         theme(plot.title = element_text(face="bold"),
+               axis.title.x=element_blank(),
+               axis.text.x=element_blank(),
+               axis.ticks.x=element_blank())+
+         guides(fill = guide_colourbar(barwidth = 1, barheight = 10))+
+         ylab("Timescale (mos)")+
+         labs(title=paste0(titleName," Standardized Precipitation-Evapotranspiration Index (", format(as.Date(date1), "%b%Y"),
+                           " - ",format(as.Date(date2), "%b%Y"),")"))+
+         theme(plot.margin = unit(c(5, 0, 0, 0), "pt"))
+       
+       # precip anoms
+       p2<- ggplot(tempDataMelt,aes(date, P_PETAnom, fill = petAnomSign)) + 
+         geom_bar(stat = "identity", show.legend = FALSE) + 
+         #scale_y_continuous(breaks = seq(-100, 100, 20)) +
+         scale_fill_manual(values = c("orange4", "darkgreen"))+
+         scale_x_date(labels = date_format("%Y"), breaks='2 years', expand=c(0,0),
+                      limits = c(as.Date(date1),as.Date(date2)))+
+         ylab("P-PET Anom (in.)")+
+         xlab("Month-Year")+
+         theme_bw()+
+         theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+         theme(plot.margin = unit(c(0, 0, 0, 0), "pt"))+
+         theme(panel.grid.minor = element_blank())
+       
+       # # trying to get alignments
+       #    mainP<-plot_grid(p1, p2, ncol = 1, align = 'v', axis=c('l'),rel_heights = c(3.5,1))
+       #    sideP<-plot_grid(pCurr, NULL, ncol = 1, rel_heights = c(3.5,1))
+       #    plot_grid(mainP, sideP, nrow = 1, align='h',axis = c('tblr'), rel_widths = c(20,1))
+       # # another solution
+       #    plot_grid(p1,pCurr,p2,NULL, ncol = 2, nrow = 2, align = 'v',axis = 'b', rel_heights = c(10,10,1,1),
+       #              rel_widths = c(20,1,20,1))
+       
+       # plotting grid using align
+       mainCurr <- align_plots(p1, pCurr, align = 'h', axis = 'l')
+       mainPrec <- align_plots(p1, p2, align = 'v', axis = 'b')
+       
+       mainP<-plot_grid(mainCurr[[1]], mainPrec[[2]], ncol = 1, align = 'v', axis=c('l'),rel_heights = c(3.5,1))
+       sideP<-plot_grid(pCurr, NULL, ncol = 1, rel_heights = c(3.5,1))
+       #plot_grid(mainP, sideP, nrow = 1, align='h',axis = c('tblr'), rel_widths = c(20,1))
+       spiPlot<-plot_grid(mainP, sideP, nrow = 1, rel_widths = c(20,1))
+       
+       # add inset map
+       spiPlot<-ggdraw(spiPlot)+draw_plot(insetmap, -0.315, 0.40, scale=0.14)
+       
+       # add margin
+       spiPlot = spiPlot + theme(plot.margin = unit(c(0.25, 0.25, 0.7, 0.25), "in")) 
+       # add caption
+       captionString <- c( "Data from NOAA-NCEI",
+                           "ftp://ftp.ncdc.noaa.gov/pub/data/cirs/climdiv/",
+                           paste0("Plot created: ", format(Sys.Date(), "%m-%d-%Y")),
+                           "The University of Arizona",
+                           "https://cals.arizona.edu/climate/")
+       spiPlot<-ggdraw(spiPlot) + draw_text(captionString, x =0.125, 
+                                            y = c(0.0625,0.0500,0.0375,0.0250,0.0125), hjust = 0,vjust=-0.25, size=8)
+       
+       
+       # write high res to file ----
+       png("/home/crimmins/RProjects/ClimPlot/spiPlots/speiPlots.png", width = 11, height = 8.5, units = "in", res = 300L)
+       #grid.newpage()
+       print(spiPlot, newpage = FALSE)
+       dev.off()
+       
+       # add logos
+       # Call back the plot
+       plot <- image_read("/home/crimmins/RProjects/ClimPlot/spiPlots/speiPlots.png")
+       # And bring in a logo
+       #logo_raw <- image_read("./logos/UA_CLIMAS_logos.png") 
+       logo_raw <- image_read("/home/crimmins/RProjects/ClimPlot/logos/UA_CSAP_CLIMAS_logos_horiz.png") 
+       logo <- image_resize(logo_raw, geometry_size_percent(width=95,height = 95))
+       # Stack them on top of each other
+       #final_plot <- image_append((c(plot, logo)), stack = TRUE)
+       #final_plot <- image_mosaic((c(plot, logo)))
+       final_plot <- image_composite(plot, logo, offset = "+2235+2365")
+       # And overwrite the plot without a logo
+       image_write(final_plot, "/home/crimmins/RProjects/ClimPlot/spiPlots/speiPlots.png")  
+       # ----   
+       
+       # PLOTLY SPI HEATMAP ----
+       plot_ly(dfSPEI, x = ~date, y = ~variable, z = ~value, colors='BrBG', type = "heatmap", zmin=-3, zmax=3) %>%
+         layout(title = paste0(titleName," Standardized Precipitation-Evapotranspiration Index (", format(as.Date(date1), "%b%Y"),
+                               " - ",format(as.Date(date2), "%b%Y"),")"),
+                xaxis=list(title="Month-Year",
+                           range = c(as.Date(date1),as.Date(date2))),
+                yaxis=list(title="Timescale (mos)",
+                           range = c(0,maxScale))
+         )
+       # ----         
     
-    # SPEI
-    test2<-melt(dfSPEI, id.vars=c(1:3), measure.vars = 4:63)
-    test2$value<-as.numeric(test2$value)
-    colnames(test2)[2]<-"month"
-    ggplot(test2, aes((date),as.numeric(variable) , fill = value))+
-      geom_raster()+
-      scale_fill_gradient2(low = "brown", high = "green",mid = "white",
-                           na.value = "grey50", guide = "colourbar")
+       # PLOTLY diff plot
+       tempPlotlyDF<-as.data.frame(cbind(dfSPI$variable,dfSPI$value-dfSPEI$value))
+        colnames(tempPlotlyDF)<-c("variable","value")
+        tempPlotlyDF$date<-dfSPI$date
+       plot_ly(tempPlotlyDF, x = ~date, y = ~variable, z = ~value, colors='PuOr', type = "heatmap", zmin=-3, zmax=3) %>%
+          layout(title = paste0(titleName," SPI-SPEI (", format(as.Date(date1), "%b%Y"),
+                                " - ",format(as.Date(date2), "%b%Y"),")"),
+                 xaxis=list(title="Month-Year",
+                            range = c(as.Date(date1),as.Date(date2))),
+                 yaxis=list(title="Timescale (mos)",
+                            range = c(0,maxScale))
+          )  
+        
+        
+        # interactive plots of temp, precip, PET, Anoms
     
-    # diff plot
-    test2$diff<-test$value-test2$value
-    ggplot(test2, aes((date),as.numeric(variable) , fill = diff))+
-      geom_raster()+
-      scale_fill_gradient2(low = "blue", high = "red",mid = "white",
-                           na.value = "grey50", guide = "colourbar")    
-    
-    
-    # metR contour plot
-    #ggplot(test, aes((date),as.numeric(variable) , z = value))+
-    #  geom_contour_fill()
+
 
 
